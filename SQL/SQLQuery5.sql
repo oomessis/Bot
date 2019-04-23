058e37bfcb848d6a8e92c5b27dd75027a44e23c5
Janne Papula <raybarg@gmail.com>
2019-04-23 16:31:47 +0300

    SQL Cleanup.

--- SQL/SQLQuery5.sql
+++
@@ -1,5 +0,0 @@
-select count(*) maara, person_name from discord_messages 
-group by person_name
-order by count(*) desc
-
-select * from discord_messages where person_name = '' order by message_date desc
